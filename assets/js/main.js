b262715c86e41988567e0a535bd8ff97739dbada
Hugo Leonardo Usqueda <leonardo@iurco.com>
2022-10-11 22:54:10 -0300

    2da pre-entrega proyecto final

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,30 +2,45 @@
 
 const juego = () => {
 
+    function game( players, max ){
+        this.players = players;
+        this.max = max;
+    }
+    
     let players = parseInt( prompt(`Ingrese la cantidad de jugadores.`) );
     let max = parseInt( prompt(`Ingrese la cantidad de numeros por jugador.`) );
+
+    let config = new game (players,max);
+    
     let tabla = [];    
 
-    for ( let i=1; i <= players; i++ ){
+    console.log(config);
+
+    for ( let i=1; i <= config.players; i++ ){
         let nick = prompt(`Jugador ${i}, ingrese su nick.`);
         if ( isNaN( nick ) ) {
-            tabla.push(nick);
-            tabla[1][i] = llenarCarton(tabla,max);
+            tabla.push(llenarCarton(nick,config));
         }
     }
 
-    console.table(tabla);
+    console.log(tabla);
 
 }
 
-const llenarCarton = (jugador,max) => {
-    for ( let j=1; j <= max; j++ ) {
+const llenarCarton = (nick,config) => {
+
+    let jugador = new Object();
+    jugador.nick = nick;
+    jugador.numeros = [];
+
+    for ( let j=1; j <= config.max; j++ ) {
         let num = parseInt( prompt(`Ingrese el ${j} numero.`) );
         if ( verificarNumero(num) == true ) {
-            jugador.push(num);
+            jugador.numeros.push(num);
         }
     }   
-    
+
+    console.log(jugador);    
     return jugador;
 }
 
